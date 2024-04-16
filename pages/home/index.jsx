3c4262390601f18d7422717a9d042892422763a8
Saagcode <146259869+Saagcode@users.noreply.github.com>
2024-04-16 10:04:09 -0300

    bug fix download file

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -11,30 +11,30 @@ function home() {
   function changeIcon() {
     setColor(!color);
   }
-  function downloadFile() {
-    // Crie os dados do arquivo
-    const filePath = "../../images/curriculo_GabrielSabino.pdf";
+  // function downloadFile() {
+  //   // Crie os dados do arquivo
+  //   const filePath = "../../images/curriculo_GabrielSabino.pdf";
 
-    fetch(filePath)
-      .then(response => response.blob())
-      .then(blob => {
-        // Crie um URL temporário para o Blob
-        const url = URL.createObjectURL(blob);
+  //   fetch(filePath)
+  //     .then(response => response.blob())
+  //     .then(blob => {
+  //       // Crie um URL temporário para o Blob
+  //       const url = URL.createObjectURL(blob);
 
-        // Crie um link <a> para o URL e simule um clique para iniciar o download
-        const a = document.createElement('a');
-        a.href = url;
-        const mimeType = blob.type;
-        a.download = 'curriculo_GabrielSabino'; // Nome do arquivo para download
-        a.type = mimeType
-        document.body.appendChild(a);
-        a.click();
+  //       // Crie um link <a> para o URL e simule um clique para iniciar o download
+  //       const a = document.createElement('a');
+  //       a.href = url;
+  //       const mimeType = blob.type;
+  //       a.download = 'curriculo_GabrielSabino'; // Nome do arquivo para download
+  //       a.type = mimeType
+  //       document.body.appendChild(a);
+  //       a.click();
 
-        // Limpe o URL temporário
-        URL.revokeObjectURL(url);
-      })
-      .catch(error => console.error('Erro ao baixar o arquivo:', error));
-  }
+  //       // Limpe o URL temporário
+  //       URL.revokeObjectURL(url);
+  //     })
+  //     .catch(error => console.error('Erro ao baixar o arquivo:', error));
+  // }
 
   const [curriculum, setCurriculum] = useState(false);
   function showCurriculum() {
@@ -76,14 +76,36 @@ function home() {
                     <div className='btn-organization'>
                       <div className='container-btn-projects'>
                         <Link to='/portfolio'>
-                          <button className='btn-projects' onMouseEnter={showPic} onMouseOut={hiddenPic}>
-                            Meus Projetos <span className='fa-solid fa-laptop-code' onMouseEnter={showPic} onMouseOut={hiddenPic} />
+                          <button
+                            className='btn-projects'
+                            onMouseEnter={showPic}
+                            onMouseOut={hiddenPic}
+                          >
+                            Meus Projetos
+                            <span
+                              className='fa-solid fa-laptop-code'
+                              onMouseEnter={showPic}
+                              onMouseOut={hiddenPic}
+                            />
                           </button>
                         </Link>
                       </div>
-                      <button onClick={() => downloadFile()} className='btn-cv' onMouseEnter={showCurriculum} onMouseOut={hiddenCurriculum}>
-                        Download CV <span className='fa-solid fa-download' onMouseEnter={showCurriculum} onMouseOut={hiddenCurriculum} />
-                      </button>
+                      <a href="../../images/curriculo_GabrielSabino.pdf" download={'curriculo_GabrielSabino'}>
+
+                        <button
+                          // onClick={() => downloadFile()}
+                          className='btn-cv'
+                          onMouseEnter={showCurriculum}
+                          onMouseOut={hiddenCurriculum}
+                        >
+                          Download CV
+                          <span
+                            className='fa-solid fa-download'
+                            onMouseEnter={showCurriculum}
+                            onMouseOut={hiddenCurriculum}
+                          />
+                        </button>
+                      </a>
                     </div>
                     {showPicture && (
                       <div style={{ display: 'flex', justifyContent: 'center', opacity: '0.7' }}>
